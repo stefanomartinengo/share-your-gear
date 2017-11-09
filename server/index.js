ef27ec2f24a909d2f6a6fe3ef1178fd34772a4e7
Stefano Martinengo <stefanomartinengo3@gmail.com>
2017-11-09 12:25:34 -0700

    changed auth endpoint

--- server/index.js
+++ server/index.js
@@ -68,7 +68,7 @@ passport.deserializeUser(function (id, done) {
 
 //endpoints//
 
-app.get('/', passport.authenticate('auth0'))
+app.get('/auth', passport.authenticate('auth0'))
 app.get('/auth/callback', passport.authenticate('auth0', {
   successRedirect: 'http://localhost:3000/#/search',
   failureRedirect: '/'
